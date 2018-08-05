dabe6bf8d1be59331063a5c4b1f31f6ced08c7b0
benchen0955 <cochu55@yahoo.com.tw>
2018-08-05 11:25:54 +0800

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,35 @@
+
+from smtplib import SMTP, SMTPAuthenticationError, SMTPException
+from email.mime.text import MIMEText
+
+host = "smtp.gmail.com"
+port = 587
+
+sender_email = 'gmail'
+receiver_email = 'to_mail'
+content_email = '你好: \n這是從 Python 發送出來的 Email, 啾咪！'
+# email='Ahua'
+pw='password' #GMAIL 帳戶設定
+msg = MIMEText(content_email.encode('utf-8'), _charset='utf-8')
+msg['Subject'] = 'Hello 你好'
+msg['From'] = "Ahua"
+msg['To'] = receiver_email
+
+conn = SMTP(host, port)
+# conn = SMTP('smtp.gmail.com:587')
+conn.ehlo()
+conn.starttls()
+try:
+    conn.login(sender_email, pw)
+    
+    conn.sendmail(sender_email,receiver_email,msg.as_string())
+    print("send ok")
+except SMTPAuthenticationError:
+    print("Could not login")
+except SMTPException:
+    print("an error occured")
+
+conn.quit()
+
+
+
